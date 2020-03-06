c40c1c89acbac8a4015227e257cd56b4a5af9878
Вершинин Павел Николаевич <pavel@impaka.ru>
2020-03-06 09:53:23 +0300

    Ошибки в шаблон не выводим

--- errors.go
+++ errors.go
@@ -14,7 +14,7 @@ type errors struct {
 func (e *errors) Add(err error) string {
 	e.s.Lock()
 	number := serialID.Next()
-	e.list = append(e.list, fmt.Sprintf("Runtime error #%d: %s\n", number, err.Error()))
+	e.list = append(e.list, fmt.Sprintf("Runtime error #%d: %s", number, err.Error()))
 	e.s.Unlock()
 	return fmt.Sprintf("[Runtime error: See #%d]", number)
 }
